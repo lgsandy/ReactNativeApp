88c8476d4b2be44a0da71853d652f2783370d554
santosh <lgsandy543@gmail.com>
2020-05-22 21:06:17 +0530

    Drawer Content is implemented.

--- App.js
+++ App.js
@@ -1,83 +1,23 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
 //import drawer
 import {createDrawerNavigator} from '@react-navigation/drawer';
 //import main tab screen
 import MainTabScreen from './screen/MainTabScreen';
-//import screens
-// import HomeScreen from './screen/HomeScreen';
-// import DetailsScreen from './screen/DetailsScreen';
-//impotr vector icon
-// import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
-// const HomeStack = createStackNavigator();
-// const DetailStack = createStackNavigator();
+//import drawer content
+import {DrawerContent} from './screen/DrawerContent';
+import SupportScreen from './screen/SupportScreen';
+import SettingsScreen from './screen/SettingsScreen';
 
 const Drawer = createDrawerNavigator();
-
-//home stack scree
-// const HomeStackScreen = ({navigation}) => {
-//   return (
-//     <HomeStack.Navigator
-//       screenOptions={{
-//         headerStyle: {
-//           backgroundColor: '#009387',
-//         },
-//         headerTintColor: '#fff',
-//       }}>
-//       <HomeStack.Screen
-//         name="Home"
-//         component={HomeScreen}
-//         options={{
-//           headerLeft: () => (
-//             <FontAwesome
-//               name="navicon"
-//               size={25}
-//               color="#fff"
-//               style={{marginLeft: 10}}
-//               onPress={() => navigation.openDrawer()}
-//             />
-//           ),
-//         }}
-//       />
-//     </HomeStack.Navigator>
-//   );
-// };
-// //details stack screen
-// const DetailStackScreen = ({navigation}) => {
-//   return (
-//     <DetailStack.Navigator
-//       screenOptions={{
-//         headerStyle: {
-//           backgroundColor: '#009387',
-//         },
-//         headerTintColor: '#fff',
-//       }}>
-//       <DetailStack.Screen
-//         name="DetailsScreen"
-//         component={DetailsScreen}
-//         options={{
-//           headerLeft: () => (
-//             <FontAwesome
-//               name="navicon"
-//               size={25}
-//               color="#fff"
-//               style={{marginLeft: 10}}
-//               onPress={() => navigation.openDrawer()}
-//             />
-//           ),
-//         }}
-//       />
-//     </DetailStack.Navigator>
-//   );
-// };
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={MainTabScreen} />
+      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        {/* <Drawer.Screen name="SupportScreen" component={SupportScreen} /> */}
+        {/* <Drawer.Screen name="SettingsScreen" component={SettingsScreen} /> */}
       </Drawer.Navigator>
     </NavigationContainer>
   );
